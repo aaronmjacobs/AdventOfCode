544162d7a6c69f36a5511371778c4a53c3a59aaf
Aaron Jacobs <aaron@aaronmjacobs.com>
2021-12-05 21:42:54 -0800

    Day 6

---
+++ 2021/6/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> lines = File.ReadLines(@"../../../input.txt").ToList();
+            List<int> fish = lines[0].Split(',').Select(int.Parse).ToList();
+
+            long[] numFish = new long[9];
+            foreach (int fishy in fish)
+            {
+                ++numFish[fishy];
+            }
+
+            for (int day = 0; day < 256; ++day)
+            {
+                long numNewFish = numFish[0];
+
+                for (int i = 0; i < numFish.Length - 1; ++i)
+                {
+                    numFish[i] = numFish[i + 1];
+                }
+
+                numFish[6] += numNewFish;
+                numFish[8] = numNewFish;
+            }
+
+            long totalFish = 0;
+            foreach (long fishy in numFish)
+            {
+                totalFish += fishy;
+            }
+
+            Console.WriteLine(totalFish);
+            Console.ReadLine();
+        }
+    }
+}
